d2e01412468c3481627841cc2ad8191ecd31d49f
pretheeviraj <pretheeviraj0805@gmail.com>
2024-12-08 16:46:00 +0530

    Updated .gitignore and API configuration

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  headers:{
-    'Content-Type': 'application/json'
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
   },
 });
 
